a88f83ead63d07ce3d9fdcfb1f8b4037f722dfb1
Harry Jones <harryrjones@yahoo.com>
2021-12-01 16:59:15 +0000

    First Versionof NameShuffler

--- src/NameShuffler/NameShuffler.java
+++ src/NameShuffler/NameShuffler.java
@@ -1,2 +1,23 @@
-package NameShuffler;public class NameShuffler {
+package NameShuffler;
+
+public class NameShuffler {
+
+    /**
+     * Method to swap two words in a string to be the other way round
+     * @param names The full name as a String
+     * @return flippedNames the two names in reverse word order
+     */
+    public String flipNames(String names){
+        //Initialise a starting string to add the names to
+        String flippedNames = null;
+
+        //Loop through each letter to find the space, which separates the two names
+        for(int i = 0; i < names.length(); i++){
+            if(names.charAt(i) == ' '){
+                //When found, add the names the other way round
+                flippedNames = names.substring(i + 1) + ' ' + names.substring(0, i);
+            }
+        }
+        return flippedNames;
+    }
 }
--- src/NameShufflerMain.java
+++ src/NameShufflerMain.java
@@ -1,11 +1,13 @@
-package NameShuffler;
+import NameShuffler.NameShuffler;
 
 public class NameShufflerMain {
 
     public static void main(String[] args) {
+        //Initialise the nameShuffler Object
         NameShuffler nameShuffler = new NameShuffler();
+
         //call flipNames
-        nameShuffler.flipNames(args[0]);
+        System.out.println(nameShuffler.flipNames(args[0]));
     }
 
 }
