33848bfe464f8f11629ebefa11c880cff72f4e5d
Harry Jones <harryrjones@yahoo.com>
2021-12-01 13:20:29 +0000

    Removed test lines

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-import com.company.Challenge1;
+import com.company.Challenge_1.Challenge1;
+import com.company.Challenge_2.Challenge2;
 public class Main {
 
     public static void main(String[] args) {
         Challenge1 challenge = new Challenge1();
+        Challenge2 challenge2 = new Challenge2();
+
 	    System.out.println(challenge.challenge1(1000));
+
+
+        String[] arrayHolder = challenge2.sortWords(challenge2.splitWords("Have a Nice Day"));
+        for(int i = 0; i < arrayHolder.length; i ++){
+            System.out.println(arrayHolder[i]);
+        }
     }
+
 
 
 }
